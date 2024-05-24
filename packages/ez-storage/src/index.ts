a1b8207d38e0fbbb2bcfd558f06b398de0092d49
raotao <>
2024-05-24 17:58:18 +0800

    feat: add async method

--- packages/ez-storage/src/index.ts
+++ packages/ez-storage/src/index.ts
@@ -10,6 +10,11 @@ type IStorageRaw<T> = IStorageOptions & {
   value: T;
 };
 
+type IAsyncStorageOptions<T, P = any> = IStorageOptions & {
+  getter?: (params: P) => Promise<T>;
+  setter?: (value: T) => Promise<T>;
+};
+
 enum TimeSpan {
   second = 1000,
   minute = 60 * 1000,
@@ -23,10 +28,10 @@ enum TimeSpan {
 const memory = new Map();
 
 const memoryStorage = Object.freeze({
-  getItem: function <T = unknown>(key: any): T | null {
+  getItem: function <T = any>(key: any): T | null {
     return memory.get(key);
   },
-  setItem: function <T = unknown>(key: any, value: T) {
+  setItem: function <T = any>(key: any, value: T) {
     memory.set(key, value);
   },
   removeItem: function (key: any): boolean {
@@ -34,13 +39,13 @@ const memoryStorage = Object.freeze({
   },
 });
 
-const getInstance = (type: IStorageType): any => {
+const getInstance = (type: IStorageType): Storage | typeof memoryStorage => {
   switch (type) {
     case 'localStorage':
     case 'sessionStorage':
-    default:
       return window[type];
     case 'memory':
+    default:
       return memoryStorage;
   }
 };
@@ -57,9 +62,9 @@ const getStorageImp = (type: IStorageType) => {
   const storage = getInstance(type);
 
   return {
-    set: function <T = unknown>(key: string, value: T): boolean {
+    set: function <T = any>(key: string, value: T): boolean {
       try {
-        storage.setItem(key, JSON.stringify(value));
+        storage.setItem(key, (type === 'memory' ? value : JSON.stringify(value)) as any);
         return true;
       } catch (error) {
         console.error('Error storing data:', error);
@@ -67,12 +72,10 @@ const getStorageImp = (type: IStorageType) => {
       }
     },
 
-    get: function <T = unknown>(key: string): T | null {
+    get: function <T = any>(key: string): T | null {
       try {
         const value = storage.getItem(key);
-        if (type === 'memory') {
-          return value;
-        }
+        if (type === 'memory') return value;
         return value ? JSON.parse(value as any) : null;
       } catch (error) {
         console.error('Error getting data:', error);
@@ -118,20 +121,62 @@ const createImp = <T>(type: IStorageType, key: string, options: IStorageOptions 
   });
 };
 
+const createCache = <T = any>(type: IStorageType, key: string, options: IStorageOptions = {}) => createImp<T>(type, key, options);
+
+const createAsync = <T>(cacheType: IStorageType, key: string, options: IAsyncStorageOptions<T> = {}) => {
+  const cache = createCache(cacheType, key, options);
+
+  const getter = typeof options.getter === 'function' ? options.getter : () => null;
+  const setter = typeof options.setter === 'function' ? options.setter : null;
+
+  return Object.freeze({
+    name: key,
+    get: async (params?: Parameters<typeof getter>) => {
+      const raw = cache.get();
+
+      if (raw) return raw;
+
+      const value = await getter(params);
+      cache.set(value);
+      return value;
+    },
+    set: async (value: T) => {
+      if (setter) await setter(value);
+      else cache.set(value);
+    },
+    remove: () => cache.remove(),
+  });
+};
+
 const createMemory = <T = any>(key: string, options: IStorageOptions = {}) => createImp<T>('memory', key, options);
-
 const createLocal = <T = any>(key: string, options: IStorageOptions = {}) => createImp<T>('localStorage', key, options);
-
 const createSession = <T = any>(key: string, options: IStorageOptions = {}) => createImp<T>('sessionStorage', key, options);
+const createAsyncLocal = <T = any>(key: string, options: IAsyncStorageOptions<T> = {}) => createAsync<T>('localStorage', key, options);
+const createAsyncSession = <T = any>(key: string, options: IAsyncStorageOptions<T> = {}) => createAsync<T>('sessionStorage', key, options);
+const createAsyncMemory = <T = any>(key: string, options: IAsyncStorageOptions<T> = {}) => createAsync<T>('memory', key, options);
 
 const EzStore = {
+  /**
+   * @description sync method
+   */
+  createCache,
   createMemory,
   createLocal,
   createSession,
+  /**
+   * @description async method
+   */
+  createAsync,
+  createAsyncLocal,
+  createAsyncSession,
+  createAsyncMemory,
+  /**
+   * @description preset timeSpan
+   */
   TimeSpan,
 };
 
 export { TimeSpan };
-export { createMemory, createLocal, createSession };
+export { createMemory, createLocal, createSession, createAsyncLocal, createAsyncSession, createAsyncMemory, createCache, createAsync };
 
 export default EzStore;
